314abcb24841fd1d73d59e5adba44e753677bf23
Smarika Adhikari <smarikaadhikari677@gmail.com>
2023-06-30 15:12:12 +0545

    bibliophile

--- lib/screens/edits.dart
+++ lib/screens/edits.dart
@@ -1,11 +1,10 @@
-import 'dart:async';
-
-import 'package:app/pages/profilepage.dart';
 import 'package:app/widgets/bio.dart';
+import 'package:app/widgets/date.dart';
 import 'package:app/widgets/new.dart';
 import 'package:flutter/material.dart';
 
 class Edits extends StatefulWidget {
+  // ignore: prefer_const_constructors_in_immutables
   Edits({super.key});
 
   @override
@@ -15,6 +14,7 @@ class Edits extends StatefulWidget {
 class _EditsState extends State<Edits> {
   TextEditingController nameEditingController = TextEditingController();
   TextEditingController bioEditingController = TextEditingController();
+  TextEditingController dateEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +28,9 @@ class _EditsState extends State<Edits> {
             width: 200,
             child: TextField(
               controller: nameEditingController,
+              decoration: const InputDecoration(
+                hintText: "Username",
+              ),
             ),
           ),
           const SizedBox(height: 10),
@@ -35,12 +38,25 @@ class _EditsState extends State<Edits> {
             width: 200,
             child: TextField(
               controller: bioEditingController,
+              decoration: InputDecoration(
+                hintText: "Bio",
+              ),
+            ),
+          ),
+          SizedBox(
+            width: 200,
+            child: TextField(
+              controller: dateEditingController,
+              decoration: InputDecoration(
+                hintText: "Date?",
+              ),
             ),
           ),
           ElevatedButton(
               onPressed: () {
                 newcontroller.add(nameEditingController.text);
                 biocontroller.add(bioEditingController.text);
+                datecontroller.add(dateEditingController.text);
               },
               child: const Text("Done"))
         ],
