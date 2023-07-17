29208653d8db9bf9486896e88440c51d9746868f
Smarika Adhikari <smarikaadhikari677@gmail.com>
2023-07-17 13:48:05 +0545

    star

--- lib/pages/firstpage.dart
+++ lib/pages/firstpage.dart
@@ -35,37 +35,26 @@ class Firstpage extends ConsumerWidget {
                       children: [
                         const Column(
                           children: [
-                            // SizedBox(height: 20, width: 30),
                             Column(
                               children: [
                                 Text("Embrace your inner bookworm...",
                                     style: TextStyle(
-                                        // color: Colors.black,
                                         fontSize: 15,
                                         fontWeight: FontWeight.bold,
                                         fontStyle: FontStyle.italic)),
                                 Text("with",
                                     style: TextStyle(
-                                        // color: Colors.black,
-                                        fontSize: 10,
-                                        fontWeight: FontWeight.bold,
-                                        fontStyle: FontStyle.italic)),
-                                Text("THE BOOK STACK",
-                                    style: TextStyle(
-                                        // color: Colors.black,
-                                        fontSize: 15,
-                                        fontWeight: FontWeight.bold,
-                                        fontStyle: FontStyle.italic)),
-                                // sizedBox(height: 20, width: 30),
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.bold,
+                                    )),
                                 Image(
                                     image: AssetImage('view/BSS.png'),
-                                    height: 50,
-                                    width: 80),
+                                    height: 70,
+                                    width: 100),
                               ],
                             ),
                           ],
                         ),
-                        // const SizedBox(height: 20, width: 30),
                         Image.asset(
                           'view/kitabalaya.png',
                           fit: BoxFit.cover,
