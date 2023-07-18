db0689cdba8c183f5cf48bc366bbc34b2dadd356
Smarika Adhikari <smarikaadhikari677@gmail.com>
2023-07-18 07:20:53 +0545

    mangal

--- lib/provider/payment_summary.dart
+++ lib/provider/payment_summary.dart
@@ -3,15 +3,8 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class ExpansionStateNotifier extends StateNotifier<String> {
   ExpansionStateNotifier() : super("choose");
-
-  //set string
-  // void setPayment(String payment) {
-  //   print("gayo");
-  //   state = state;
-  // }
 }
 
-// do uing change notifier
 class PaymentNotifier extends ChangeNotifier {
   String _payment = "choose";
 
