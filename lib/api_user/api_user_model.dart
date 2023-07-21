3ed6393d8caf2a83345b910062937f621cc1306e
Smarika Adhikari <smarikaadhikari677@gmail.com>
2023-07-21 14:04:11 +0545

    saaa

---
+++ lib/api_user/api_user_model.dart
@@ -0,0 +1,38 @@
+class User {
+ final String id;
+ final String email;
+ final String name;
+ final String password;
+ final String phone;
+  final String address;
+
+
+
+
+
+
+
+  User({
+    required this.id,
+    required this.email,
+    required this.name,
+    required this.password,
+    required this.phone,
+    required this.address,
+
+  });
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      email: json['email'],
+      name: json['name'],
+      password: json['password'],
+      phone: json['phone'],
+      address: json['address'],
+   
+    );
+  } 
+  
+
+
+}
