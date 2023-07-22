d963c078a9b78db78494f1bbab0aca38032493df
Smarika Adhikari <smarikaadhikari677@gmail.com>
2023-07-22 21:08:19 +0545

    sat

--- lib/api/api_service.dart
+++ lib/api/api_service.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: unused_local_variable
+
 import 'dart:convert';
 
 import 'package:app/api/api_model.dart';
@@ -12,13 +14,13 @@ class ApiService {
   Future<List<Books>> getProducts() async {
     const url = "/book/getallbooks";
     try {
-<<<<<<< HEAD
+// <<<<<<< HEAD
       final res = await Api().get(MyConfig.appUrl + url);
       List data = json.decode(res.data);
-=======
-      final res = await Dio().get('http://192.168.1.68:8080/book/getallbooks');
-      List data = res.data;
->>>>>>> ee8e789361ddcfa0d50152bc413d511fd45ddfc2
+// =======
+//       final  = await Api().get('http://192.168.1.68:8080/book/getallbooks');
+//       List data = res.data;
+// >>>>>>> ee8e789361ddcfa0d50152bc413d511fd45ddfc2
       return data.map((e) => Books.fromJson(e)).toList();
     } catch (e) {
       throw Exception('Error getting suggestion $e');
